76d056d7553ff61ca3d0a0c02917a339e4c2ff9b
Nathan Scott <dev@ngscott.net>
2020-05-26 15:54:18 +1000

    Remove scope from webpack output

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,10 @@ const baseConfig = require('@swup/webpack-config');
 const name = require('./package.json').name;
 const upperFirst = require('lodash/upperFirst');
 const camelCase = require('lodash/camelCase');
+const last = require('lodash/last');
 
-const PascalCaseName = upperFirst(camelCase(name));
+const unscoped = last(name.split('/'))
+const PascalCaseName = upperFirst(camelCase(unscoped));
 
 const config = Object.assign({}, baseConfig, {
 	entry: {
